370e925d2842d146a374bf70f07f81e97d46c29b
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-13 13:44:59 -0500

    delete Tania Rascia example code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
+      Hi there
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
